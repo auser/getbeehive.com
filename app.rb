f8ab9f73afda5b2436db4eee551defc44f536844
Ari Lerner <arilerner@mac.com>
2009-09-23 19:34:15 -0700

    Added ps

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ require 'sinatra'
 get '/' do
   dir = `pwd`
   env = ENV.map {|k,v| "     #{k} => #{v}<br />"}
-  ps = `ps aux`
+  ps = `ps aux`.split("\n").join("<br />")
   
   "#{dir}<br />#{env}<br />#{ps}"
 end
