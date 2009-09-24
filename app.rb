0dc03e055db112f121814ab86aa962cd01f11e8c
Ari Lerner <arilerner@mac.com>
2009-09-23 19:39:19 -0700

    Added p

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ get '/' do
   dir = `pwd`
   env = ENV.map {|k,v| "     #{k} => #{v}<br />"}
   ps = `ps aux`.split("\n").join("<br />")
-  log = `tail log/server_9228_ca0e3d7_3410_17777.log`
+  log = `ls log/`.split("\n").join("<br />")
   
   "#{dir}<br />#{env}<br />#{ps}<br />#{log}"
 end
