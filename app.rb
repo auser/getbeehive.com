dc95ae3cb1258de45f6d7fac0a63422df883483f
Ari Lerner <arilerner@mac.com>
2009-09-23 19:35:59 -0700

    Added p

--- app.rb
+++ app.rb
@@ -5,6 +5,8 @@ get '/' do
   dir = `pwd`
   env = ENV.map {|k,v| "     #{k} => #{v}<br />"}
   ps = `ps aux`.split("\n").join("<br />")
+  service_mon = `cat /usr/local/bin/service_monitor`
+  thin = `cat /usr/local/bin/thin_launcher`
   
-  "#{dir}<br />#{env}<br />#{ps}"
+  "#{dir}<br />#{env}<br />#{ps}<br />#{service_monitor}<br />#{thin}"
 end
