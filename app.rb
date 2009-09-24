18877befdb6436ec06c42ec379e4655c22eba507
Ari Lerner <arilerner@mac.com>
2009-09-23 19:36:39 -0700

    Added p

--- app.rb
+++ app.rb
@@ -5,7 +5,6 @@ get '/' do
   dir = `pwd`
   env = ENV.map {|k,v| "     #{k} => #{v}<br />"}
   ps = `ps aux`.split("\n").join("<br />")
-  service_mon = `cat /usr/local/bin/service_monitor`
   
-  "#{dir}<br />#{env}<br />#{ps}<br />#{service_monitor}"
+  "#{dir}<br />#{env}<br />#{ps}<br />"
 end
