5f5db78a88e7c1914017dc8bfbef469f4358eb50
Ari Lerner <arilerner@mac.com>
2009-09-23 19:42:31 -0700

    Cleaned up

--- app.rb
+++ app.rb
@@ -4,8 +4,7 @@ require 'sinatra'
 get '/' do
   dir = `pwd`
   env = ENV.map {|k,v| "     #{k} => #{v}<br />"}
-  ps = `ps aux`.split("\n").join("<br />")
-  log = `ls log/`.split("\n").join("<br />")
+  # ps = `ps aux`.split("\n").join("<br />")
   
-  "#{dir}<br />#{env}<br />#{ps}<br />#{log}"
+  "#{dir}<br />#{env}<br />"
 end
