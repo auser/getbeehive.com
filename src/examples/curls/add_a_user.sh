36bc01e185cc85fda9e693c439483e3138bd5e6d
Ari Lerner <arilerner@mac.com>
2009-12-10 16:13:24 -0800

    Updating site

---
+++ src/examples/curls/add_a_user.sh
@@ -0,0 +1,7 @@
+HTTP/1.1 200 OK
+Server: MochiWeb/1.0 (Any of you quaids got a smint?)
+Date: Fri, 11 Dec 2009 00:07:06 GMT
+Content-Type: text/json
+Content-Length: 28
+
+{"user":"arilerner@mac.com"}
---
+++ src/examples/curls/cleanup_root_user.sh
@@ -0,0 +1,7 @@
+HTTP/1.1 200 OK
+Server: MochiWeb/1.0 (Any of you quaids got a smint?)
+Date: Fri, 11 Dec 2009 00:10:05 GMT
+Content-Type: text/json
+Content-Length: 4
+
+"ok"
